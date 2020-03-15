1209e26a99676df2e78b1ed0b918a5fd5bdc6d9e
hammu116 <59144819+hammu116@users.noreply.github.com>
2020-03-15 11:53:10 +0530

    Add files via upload

---
+++ patterns/src/main/java/Epam/patterns/App.java
@@ -0,0 +1,13 @@
+package Epam.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
